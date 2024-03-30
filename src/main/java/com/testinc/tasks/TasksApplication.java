1d9582be5c632fdfb64e9d5b44856594befbf969
Prashanth Reddy <lprashanthreddy9@gmail.com>
2024-03-30 10:52:54 -0500

    Update TasksApplication.java

--- src/main/java/com/testinc/tasks/TasksApplication.java
+++ src/main/java/com/testinc/tasks/TasksApplication.java
@@ -3,6 +3,7 @@ package com.testinc.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import com.testinc.tasks.config.CorsConfig;
 
 @SpringBootApplication
 @Import(CorsConfig.class)
